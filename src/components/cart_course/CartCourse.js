2fcfcd81bf505d1e6e5355f281509edc338fbbf5
Ali <alimahmoudi637@gmail.com>
2022-02-20 22:33:45 +0330

    Added some new style to Cart Course component

--- src/components/cart_course/CartCourse.js
+++ src/components/cart_course/CartCourse.js
@@ -4,18 +4,19 @@ import './Cart.css';
 
 function CartCourse(){
     return(
-        <div className="container">
-            <img src="https://www.thinkific.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/06/Create-Online-Courses-10.jpg.webp" alt="#"/>
-            <p>
-                Learning Python for Data Analysis and Visualization
-            </p>
-            <p>Score</p>
-            <Rating
-                name='simple-controlled'
-                value={2.5}
-                precision={0.5}
-            />
-        </div>
+            <div className="container">
+                <img src="https://www.thinkific.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/06/Create-Online-Courses-10.jpg.webp" alt="#"/>
+                <p style={{margin:0,fontWeight:'bold'}}>
+                    Learning Python for Data Analysis and Visualization
+                </p>
+                <p style={{margin:0,fontWeight:'lighter',fontSize:12}}>Ali Mahmoudi</p>
+                <p style={{margin:0}}>Score</p>
+                <Rating
+                    name='simple-controlled'
+                    value={2.5}
+                    precision={0.5}
+                />
+            </div>
     );
 }
 
