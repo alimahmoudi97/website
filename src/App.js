0ee121869ca690f13d8c7c2031af384591e9eabb
Ali <alimahmoudi637@gmail.com>
2022-02-22 19:26:01 +0330

    Added Slider to App.js to customize Slider carts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Header from './components/header/Header'
 import Introduce from './components/Introduce/Introduce';
 import Courses from './components/courses/Courses';
 import CartCourse from './components/cart_course/CartCourse';
+import SliderCart from './components/slider/Slider';
 function App() {
   return (
-    <div>
-      <Header/>
-      <Introduce/>
-      <Courses/>
-    </div>
+    <div style={{backgroundColor:'gray'}}>
+    {/* <Header/>
+    <Introduce/>
+    <Courses/> */}
+    <div style={{width:'90%',marginLeft:70}}>
+      <SliderCart/>
+    </div>    
+  </div>
 
   );
 }
