9b3de1ba2ca6cd46ec4b7156c11e4affd956bb4b
Ali <alimahmoudi637@gmail.com>
2022-02-22 23:58:42 +0330

    customiz arrow buttons

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,9 +4,34 @@ import Slider from "react-slick";
 import "./../../../node_modules/slick-carousel/slick/slick.css"; 
 import "./../../../node_modules/slick-carousel/slick/slick-theme.css";
 
+import {BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill} from 'react-icons/bs';
+
 import CartCourse from './../cart_course/CartCourse';
-// import './Slider.css';
+import './Slider.css';
 function SliderCart(){
+
+    const SlickArrowLeft=(props)=>{
+        const {className,style,onClick,currentSlide}=props;
+        return(
+            // <img  src='./../../assets/left_icon.jpg' alt='prevArrow' {...props}/>
+            <div onClick={onClick} className="arrow-left">
+                <BsFillArrowLeftCircleFill style={{width:50,height:50}} />
+            </div>
+            
+        )
+        
+    };
+    const SlickArrowRight=(props)=>{
+        const {className,style,onClick,currentSlide}=props;
+        return(
+            // <img  src='./../../assets/left_icon.jpg' alt='prevArrow' {...props}/>
+            <div onClick={onClick} className="arrow-right">
+                <BsFillArrowRightCircleFill style={{width:50,height:50}}/>
+            </div>
+            
+        )
+        
+    };
     const settings = {
         dots: true,
         infinite: false,
@@ -15,13 +40,24 @@ function SliderCart(){
         slidesToScroll: 5,
         initialSlide: 0,
         swipeToSlide:false,
+        // centerMode:true,
         // dotsClass:'dots',
         // vertical:false,
-        // className:'items'
+        // className:"center",
+        // centerPadding: "160px",
+        prevArrow:<SlickArrowLeft/>,
+        nextArrow:<SlickArrowRight/>,
       };
     return(
-        <div>
-            <Slider {...settings}>
+            
+            <Slider {...settings} >
+                {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].map(()=>{
+                    return(
+                        <div>
+                            <CartCourse/>
+                        </div>
+                    )
+                })}
                 <div>
                     <CartCourse/>
                 </div>
@@ -53,7 +89,7 @@ function SliderCart(){
                     <CartCourse/>
                 </div>
             </Slider>
-        </div>
+        
     )
 }
 
