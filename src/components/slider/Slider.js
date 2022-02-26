9d76a1752c48f2ebb9c6c06095f113563688a88c
Ali <alimahmoudi637@gmail.com>
2022-02-27 00:41:52 +0330

    modify and customized slider swiper

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,32 +1,69 @@
 import React from 'react';
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
-import "swiper/swiper.scss";
-import "swiper/modules/navigation/navigation.scss";
-import "swiper/modules/pagination/pagination.scss";
+// import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "./../../../node_modules/swiper/swiper.scss";
+import "./../../../node_modules/swiper/modules/navigation/navigation.scss";
+import "./../../../node_modules/swiper/modules/pagination/pagination.scss";
+import "./Slider.css";
+import SwiperCore,{ Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import {BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill} from 'react-icons/bs'
+import CartCourse from './../cart_course/CartCourse';
+SwiperCore.use([Navigation]);
 function SliderCart(){
-
+    const navigationPrevRef = React.useRef(null)
+    const navigationNextRef = React.useRef(null)
+  
     return(
-        <div>
+        <div className="slider-content">
+          {/* <BsFillArrowLeftCircleFill ref={navigationPrevRef} style={{zIndex:1,backgroundColor:'red',width:50,height:50}}/> */}
+          <div ref={navigationPrevRef} style={{position:'relative',top:120,left:20,zIndex:2,color:'green',width:50,height:50}}> <BsFillArrowLeftCircleFill style={{width:'100%',height:'100%'}}/> </div>
             <Swiper
-                    slidesPerView={3}
-                    spaceBetween={30}
-                    pagination={{
-                    clickable: true,
-                    }}
-                    // modules={[Pagination]}
-                    
+               slidesPerView={5}
+               spaceBetween={25}
+            //    slidesPerGroup={5}
+               preloadImages={false}
+            //    slidesOffsetAfter={-100}
+            //    slidesOffsetBefore={-200}
+            //    effect="fade"
+            //    loop={true}
+            //    loopFillGroupWithBlank={true}
+               pagination={{
+                 clickable: true,
+               }}
+               onSwiper={(swiper) => {
+                // Delay execution for the refs to be defined
+                setTimeout(() => {
+                  // Override prevEl & nextEl now that refs are defined
+                  swiper.params.navigation.prevEl = navigationPrevRef.current
+                  swiper.params.navigation.nextEl = navigationNextRef.current
+        
+                  // Re-init navigation
+                  swiper.navigation.destroy()
+                  swiper.navigation.init()
+                  swiper.navigation.update()
+                })
+              }}
+               modules={[Navigation]}
+               className="mySwiper"
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    <SwiperSlide>Slide 5</SwiperSlide>
-                    <SwiperSlide>Slide 6</SwiperSlide>
-                    <SwiperSlide>Slide 7</SwiperSlide>
-                    <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
+
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    <SwiperSlide><CartCourse/></SwiperSlide>
+                    
+                    {/* <div ref={navigationNextRef} style={{position:'relative',top:50}}> <p>prev</p></div> */}
                 </Swiper>
+                {/* <BsFillArrowRightCircleFill ref={navigationNextRef} style={{zIndex:1,backgroundColor:'red',width:50,height:50}}/> */}
+                <div ref={navigationNextRef} style={{position:'relative',top:120,right:20,zIndex:1,color:'green',width:50,height:50}}> <BsFillArrowRightCircleFill style={{width:'100%',height:'100%'}}/> </div>
         </div>
     )
 }
