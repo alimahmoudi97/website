d60fdb23034ddd9168e6301f65845c15bd4d0fe4
Ali <alimahmoudi637@gmail.com>
2022-03-05 20:00:38 +0330

    add data array to Categories.js

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,25 +1,60 @@
 import React from "react";
 import './Categories.css';
 
-function Cart(){
+const data=[
+    {
+        name:"Design",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-design-v2.jpg"
+    },
+    {
+        name:"Development",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-development-v2.jpg"
+    },
+    {
+        name:"Marketing",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-marketing-v2.jpg"
+    },
+    {
+        name:"IT and Software",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-it-and-software-v2.jpg"
+    },
+    {
+        name:"Personal Development",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-personal-development-v2.jpg"
+    },
+    {
+        name:"Business",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-business-v2.jpg"
+    },
+    {
+        name:"Photography",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-photography-v2.jpg"
+    },
+    {
+        name:"Music",
+        image:"https://s.udemycdn.com/home/top-categories/lohp-category-music-v2.jpg"
+    },
+];
+
+function Cart(props){
     return(
         <div>
-            <img src="https://s.udemycdn.com/home/top-categories/lohp-category-design-v2.jpg"/>
-            <p style={{margin:0}}>Design</p>
+            <img src={props.item.image}/>
+            <p style={{margin:0}}>{props.item.name}</p>
         </div>
     )
 }
 function Categories(){
     return(
         <div className="grid-container">
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
-            <div className="grid-item"><Cart/></div>
+            <div className="grid-item"><Cart item={data[0]}/></div>
+            <div className="grid-item"><Cart item={data[1]}/></div>
+            <div className="grid-item"><Cart item={data[2]}/></div>
+            <div className="grid-item"><Cart item={data[3]}/></div>
+            <div className="grid-item"><Cart item={data[4]}/></div>
+            <div className="grid-item"><Cart item={data[5]}/></div>
+            <div className="grid-item"><Cart item={data[6]}/></div>
+            <div className="grid-item"><Cart item={data[7]}/></div>
         </div>
     )
 }
