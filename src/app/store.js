57cf2f6766d0fa7ba4e1ae0a0b474a95adce7d81
alimahmoudi97 <alimahmoudi637@gmail.com>
2022-06-27 09:33:02 +0430

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import buyCoursesReducer from './buyCoursesSlice';
-
-export default configureStore({
-    reducer:{
-        counter:buyCoursesReducer
-    }
-})
